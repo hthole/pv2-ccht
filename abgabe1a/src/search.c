0eee42e8d34c9396686dd1f66ee17009fbbe97e5
just933ks <just933ks@d7a55080-4514-11de-a3b0-c1e83b3b5a46>
2009-05-25 12:54:00 +0000

    bool false

--- abgabe1a/src/search.c
+++ abgabe1a/src/search.c
@@ -20,10 +20,10 @@
 
 #include <assert.h>
 
-int main(int argc, char **argv)
-{
-	printf("process \n");
+int get_random_number(int);
+int find_value(int *, int, int);
 
+int main(int argc, char **argv) {
 	/* konvertiere die uebergabe parameter von char zu int	*/
 	int array_size = atoi(argv[1]);
 	int searchfor = atoi(argv[2]);
@@ -36,19 +36,15 @@ int main(int argc, char **argv)
 	char msg[20];
 	int me, total, count, tag = 99;
 	MPI_Status status;
+	MPI_Request request;
 
-	/* wie viel kriegt jeder prozess */
-	int split_size = array_size / total;
-
-	if (argc != 3)
-	{
+	if (argc != 3) {
 		printf("usage: ./search <size> <search for>\n");
 		return 1;
 	}
 
 	/* Initialisierung */
-	if (MPI_Init(&argc, &argv) != MPI_SUCCESS)
-	{
+	if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
 		printf("Konnte MPI nicht initialisieren, verlasse Programm...\n");
 		exit(1);
 	}
@@ -57,21 +53,26 @@ int main(int argc, char **argv)
 	assert (MPI_Comm_rank(MPI_COMM_WORLD, &me) == MPI_SUCCESS);
 	printf("PROCESS: %i\n", me);
 
+	/* warten, bis alle sich gemeldet haben */
+	MPI_Barrier(MPI_COMM_WORLD);
+
 	/* Wieviele von uns gibt es? */
 	assert (MPI_Comm_size(MPI_COMM_WORLD, &total) == MPI_SUCCESS);
 
+	/* wie viel kriegt jeder prozess */
+	int split_size = array_size / (total - 1);
 
-	if (me == 0)
-	{
+	if (me == 0) {
 		printf("we are %i buddies\n", total);
 
 		/* initialisieren des arrays */
-		for (i = 0; i < array_size; i++)
+		for (i = 0; i < array_size; i++) {
 			searcharray[i] = 0;
+		}
 
 		/* bestimme zufallszahl	*/
-		printf("random:%i\n", get_random_number(array_size));
 		int randnumber = get_random_number(array_size);
+		printf("random:%i\n", randnumber);
 
 		/* weise searcharray[zufallszahl] der gesuchten zahl zu (debug)	*/
 		searcharray[randnumber] = searchfor;
@@ -81,44 +82,52 @@ int main(int argc, char **argv)
 
 		int field_count = 0;
 
-		// printf("\nsplit size: %i\n", split_size);
+		printf("\nsplit size: %i\n", split_size);
 
-		for (i = 1; i <= total; i++)
-		{
+		for (i = 0; i < total; i++) {
 			int p_search_array[split_size];
 			int j;
 
-			printf("\nprocess %i:\n", i);
+			// printf("\nprocess %i:\n", i);
 
-			for (j = 0; j < split_size; j++)
-			{
+			for (j = 0; j < split_size; j++) {
 				p_search_array[j] = searcharray[field_count];
 				field_count++;
-				printf("|%i", p_search_array[j]);
+				// printf("|%i", p_search_array[j]);
 			}
 
-			printf("|");
+			//printf("|");
 
-			/*assert (MPI_Send(p_search_array, split_size + 1, MPI_INT, 1, tag,
-			                         MPI_COMM_WORLD) == MPI_SUCCESS) ;*/
-
+			assert (MPI_Isend(p_search_array, split_size + 1, MPI_INT, 1, tag,
+							MPI_COMM_WORLD, &request) == MPI_SUCCESS);
 		}
-	}
-	else
-	{
+
+	} else {
 		int p_search_array[split_size];
 
-		/*assert (MPI_Recv(p_search_array, split_size, MPI_INT, 0, tag,
-		                MPI_COMM_WORLD, &status) == MPI_SUCCESS);*/
+		assert (MPI_Irecv(&p_search_array, split_size + 1, MPI_INT, 0, tag,
+						MPI_COMM_WORLD, &status) == MPI_SUCCESS);
 
-		int result = find_value(&p_search_array, split_size, searchfor);
+		//int result = find_value(&p_search_array, split_size, searchfor);
 
-		if (result != -1)
-		{
-			printf("result : %i\n", result);
+		int z = 0;
+		for (z = 0; z < split_size; z++) {
+			printf("%i.\t%i\t%i\n", me, z, p_search_array[z]);
+			if (p_search_array[z] == 42) {
+				printf("gefunden von %i\n\n", me);
+				break;
+			}
 		}
 
+		//printf("%i: %i\n", me, result);
 	}
+
+	/*
+
+
+	 if (result != -1)
+	 break;
+	 */
 
 	/* MPI Laufzeitsystem beenden */
 	assert (MPI_Finalize() == MPI_SUCCESS);
@@ -129,17 +138,14 @@ int main(int argc, char **argv)
 }
 
 /* debug ausgabe */
-void print_debug(int *debug_info, int array_size)
-{
+void print_debug(int *debug_info, int array_size) {
 	printf("size of array: %i\n", array_size);
 
 	int i;
-	for (i = 0; i < array_size; i++)
-	{
+	for (i = 0; i < array_size; i++) {
 		printf("|%i", debug_info[i]);
 
-		if ((i + 1) % 20 == 0)
-		{
+		if ((i + 1) % 20 == 0) {
 			printf("|\n");
 		}
 	}
@@ -148,22 +154,18 @@ void print_debug(int *debug_info, int array_size)
 }
 
 /* erzeugt eine zufallszahl zwischen 0 und max_size - 1 */
-int get_random_number(int max_size)
-{
+int get_random_number(int max_size) {
 	srand(time(0));
 
 	return rand() % max_size;
 }
 
-int find_value(int *searcharray, int array_size, int search)
-{
+int find_value(int *searcharray, int array_size, int search) {
 	int i;
 	int result = -1;
 
-	for (i = 0; i < array_size; i++)
-	{
-		if (searcharray[i] == search)
-		{
+	for (i = 0; i < array_size; i++) {
+		if (searcharray[i] == search) {
 			result = i;
 			break;
 		}
