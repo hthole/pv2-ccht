57c419317f9b1a9a74bb36904b3dd1b6c5f16f54
hthole@gmx.de <hthole@gmx.de@d7a55080-4514-11de-a3b0-c1e83b3b5a46>
2009-06-01 16:58:10 +0000

    latest

--- abgabe1a/src/search.c
+++ abgabe1a/src/search.c
@@ -20,7 +20,7 @@ int get_random_number(int);
 
 int main(int argc, char **argv) {
 
-	/* Deklarationen fuer parallel kram */
+	/* spaeter gebrauchte Variablen deklarieren */
 	int me, total, count, tag = 99;
 	MPI_Status status;
 	MPI_Request request;
@@ -30,16 +30,17 @@ int main(int argc, char **argv) {
 	int was_found;
 	int stop = 1337;
 	int i;
-	double start, end;
+	double start_time, end_time;
 
-	/* Initialisierung */
+	/* MPI-Initialisierung */
 	if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
 		printf("Konnte MPI nicht initialisieren, verlasse Programm...\n");
 		exit(1);
 	}
 
+	/* Parameteranzahl pruefen */
 	if (argc != 3) {
-		printf("usage: ./%s <size> <search for>\n", argv[0]);
+		printf("usage: mpiexec -n <process#> %s <size> <search for>\n", argv[0]);
 		exit(1);
 	}
 
@@ -59,18 +60,21 @@ int main(int argc, char **argv) {
 	assert(MPI_Comm_size(MPI_COMM_WORLD, &total) == MPI_SUCCESS);
 
 	if (me == ROOT) {
-		start = MPI_Wtime();
+		start_time = MPI_Wtime(); // Zeitnahme starten
 	}
 
+	/* Groesse des Suchchunks berechnen und Empfangspuffer einrichten */
 	p_buf_size = buf_size / total;
 	int recvbuf[p_buf_size];
 
 	if (me == ROOT) {
+		/* Testarray wird komplett genullt */
 		for (i = 0; i < buf_size; i++) {
 			sendbuf[i] = 0;
 		}
+		/* Zahl nach der gesucht werden soll wird zufaellig platziert */
 		int rand = get_random_number(buf_size);
-		printf("random index: %i\n", rand);
+		printf("Zufallsposition: %i\n", rand);
 		sendbuf[rand] = search_for;
 	}
 
@@ -83,39 +87,42 @@ int main(int argc, char **argv) {
 			&request) == MPI_SUCCESS);
 
 	/* Jeder Prozess testet, ob in was_found 1 (true) steht */
-	assert(MPI_Test(&request, &was_found, &status) == MPI_SUCCESS);
+	//assert(MPI_Test(&request, &was_found, &status) == MPI_SUCCESS);
 
-	printf("%i buf size %i\n", me, p_buf_size);
 
+	int j;
+	i = 0;
 	/* Jeder Prozess iteriert, suchend, ueber sein receive Array */
-	for (i = 0; !was_found && i < p_buf_size; i++) {
+	while (!was_found && i < p_buf_size) {
 		/* falls Suche erfolgreich */
 		if (recvbuf[i] == search_for) {
-			printf("gefunden an position: %i, von prozess: %i\n", (i + me
-					* p_buf_size), me);
-
-			int j;
 			/* schicke an alle Prozesse die Nachricht, dass die Suche erfolgreich war */
 			for (j = 0; j < total; j++) {
 				stop = j;
 				assert(MPI_Send(&stop, 1, MPI_INT, j, tag, MPI_COMM_WORLD)
 						== MPI_SUCCESS);
 			}
+
+			printf("gefunden an Position: %i, von Prozess: %i\n", (i + me
+								* p_buf_size), me);
 		}
 
-		/* Testen (nachfragen), ob was_found auf 1 gesetzt wurde */
+		/* Testen ob schon ein Thread den Fund gemeldet hat,
+		 *  also ob 'was_found' auf 1 gesetzt wurde
+		 */
 		assert(MPI_Test(&request, &was_found, &status) == MPI_SUCCESS);
+		i++;
 	}
 
 	if (me == ROOT) {
-		end = MPI_Wtime();
+		end_time = MPI_Wtime(); // Zeitnahme stoppen
 	}
 
 	printf("prozess %i bei position %i gestoppt.\n", me, ((i - 1) + me
 			* p_buf_size));
 
 	if (me == ROOT) {
-		printf("\nZeit gemessen: %f Sekunden\n", (end - start));
+		printf("\nZeit gemessen: %f Sekunden\n", (end_time - start_time));
 	}
 
 	/* MPI Laufzeitsystem beenden */
