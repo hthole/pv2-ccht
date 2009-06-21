6bc7841c74ff0f79017f317aa64a8dd81f8cb503
hthole@gmx.de <hthole@gmx.de@d7a55080-4514-11de-a3b0-c1e83b3b5a46>
2009-06-21 11:43:38 +0000

    asdfghjkl

--- trunk/abgabe1a/src/abgabe2a.cpp
+++ trunk/abgabe1a/src/abgabe2a.cpp
@@ -1,15 +1,15 @@
 //============================================================================
 // Name        : abgabe2a.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+#include <iostream.h>
+#include <fstream.h>
+#include <string.h>
+#include <vector.h>
 
 typedef std::vector<char> row;
 typedef std::vector<row> matrix;
@@ -17,8 +17,12 @@ typedef std::vector<row> matrix;
 int get_entry_or_exit(row entry_line);
 int find_next(matrix *maze);
 
+enum direction {
+		rechts, links, oben, unten
+	};
+
 int main() {
-	std::string filename = "/Users/nmrd/Devel/workspaces/uni/abgabe2a/src/bra10.txt";
+	std::string filename = "./bra10.txt";
 	std::ifstream fin;
 	matrix maze;
 
@@ -51,7 +55,7 @@ int main() {
 		std::cout << "konnte datei " << filename << " nicht finden / oeffnen" << std::endl;
 		return EXIT_FAILURE;
 	}
-	
+
 	// ----- print matrix -----
 	std::vector<char> v;
 	for (unsigned int i = 0; i < maze.size(); i++) {
@@ -61,12 +65,12 @@ int main() {
 		}
 		std::cout << std::endl;
 	}
-	
+
 	std::cout << "eingang: " << get_entry_or_exit(maze.at(0)) << std::endl;
 	std::cout << "ausgang: " << get_entry_or_exit(maze.at(maze.size() - 1)) << std::endl;
-	
+
 	find_next(&maze);
-	
+
 	return EXIT_SUCCESS;
 }
 
@@ -77,14 +81,14 @@ int get_entry_or_exit(row line) {
 			return i;
 		}
 	}
-	
+
 	return -1;
 }
 
 int find_next(matrix *maze) {
 	unsigned int j = 0;
 	unsigned int k = 5;
-	
+
 	if (k < (maze->size() - 1) && maze->at(j).at(k + 1) == ' ') {
 		std::cout << "rechts gehts weiter " << std::endl;
 	}
@@ -98,3 +102,62 @@ int find_next(matrix *maze) {
 		std::cout << "unten gehts weiter " << std::endl;
 	}
 }
+
+
+
+
+void walk_maze (int x_pos, int y_pos, direction dir_to_walk, int last_step_count) {
+
+	/* prüfen ob Kreuzung vorliegt */
+	int check_ways = 0;
+	int right = 0, left = 0, up = 0, down = 0;
+	if (x_pos - 1 != '#') {
+		check_ways++;
+		left = 1;
+	}
+	if (x_pos + 1 != '#') {
+		check_ways++;
+		right = 1;
+	}
+	if (y_pos - 1 != '#') {
+		check_ways++;
+		up = 1;
+	}
+	if (y_pos + 1 != '#') {
+		check_ways++;
+		down = 1;
+	}
+	if (check_ways > 2) {
+		/*
+		 * punkt zur queue hinzufügen
+		 * mögliche richtungen mitgeben
+		 * methode beenden
+		 */
+		if (left == 1)
+			add_queue (x_pos, y_pos, links, last_step_count + 1);
+		if (right == 1)
+			add_queue (x_pos, y_pos, rechts, last_step_count + 1);
+		if (up == 1)
+			add_queue (x_pos, y_pos, oben, last_step_count + 1);
+		if (down == 1)
+			add_queue (x_pos, y_pos, unten, last_step_count + 1);
+
+		return 0;
+	}
+
+
+	if (dir_to_walk == rechts)
+		x_pos++;
+	if (dir_to_walk == links)
+		x_pos--;
+	if (dir_to_walk == oben)
+		y_pos--;
+	if (dir_to_walk == unten)
+		y_pos++;
+
+	my_maze[x_pos][y_pos] = last_step_count + 1;
+
+	return 0;
+}
+
+
